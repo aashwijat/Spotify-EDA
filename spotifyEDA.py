a2be53cb1078bd9781e9e0425632b430de2598d3
Aashwija Thantry <aashwija.thantry@gmail.com>
2024-10-09 22:06:18 +0530

    Create spotifyEDA.py

---
+++ spotifyEDA.py
@@ -0,0 +1,18 @@
+import streamlit as st 
+import pandas as pd
+
+
+## page configuration
+st.set_page_config(layout="wide",page_title = "melody meets metrics")
+st.title(":green[Melody Meets Metrics : Spotify Data Analysis]", anchor=None)
+st.subheader(f"[Welcome Aashu!]")
+
+# # creating tabs
+tab1, tab2, tab3,tab4 = st.tabs(["Home","My Library","Listening History","Analytics"])
+
+def home():
+    st.write("#### :green[My Dashboard]")
+
+
+with tab1:
+    home()
