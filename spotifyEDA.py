c437a430c87a74fa2cc675e68992e0d9f31ccbb5
Aashwija Thantry <aashwija.thantry@gmail.com>
2024-10-14 14:48:21 +0530

    logo

--- spotifyEDA.py
+++ spotifyEDA.py
@@ -5,7 +5,8 @@ import pandas as pd
 ## page configuration
 st.set_page_config(layout="wide",page_title = "melody meets metrics")
 st.title(":green[Melody Meets Metrics : Spotify Data Analysis]", anchor=None)
-st.subheader(f"[Welcome Aashu!]")
+st.subheader(f"Welcome Aashu!")
+st.logo("logo.png")
 
 # # creating tabs
 tab1, tab2, tab3,tab4 = st.tabs(["Home","My Library","Listening History","Analytics"])
