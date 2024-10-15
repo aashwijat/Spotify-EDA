7fd0e0f6fa987bbdc812d170e30ab467d387a721
Aashwija Thantry <aashwija.thantry@gmail.com>
2024-10-15 12:23:42 +0530

    Home Update - Found Errors
    fix album names showing up encrypted

--- spotifyEDA.py
+++ spotifyEDA.py
@@ -1,6 +1,22 @@
 import streamlit as st 
 import pandas as pd
 
+#load functions
+def load_albums():
+    albums = pd.read_csv("myAlbums.csv",on_bad_lines="skip",encoding='utf-8',encoding_errors='ignore', lineterminator='\r')
+    return(albums)
+
+def load_artists():
+    artists = pd.read_csv("myArtists.csv",on_bad_lines="skip",encoding='utf-8',encoding_errors='ignore',lineterminator='\r')
+    return(artists)
+
+def load_tracks():
+    tracks = pd.read_csv("myTracks.csv",on_bad_lines="skip",encoding='utf-8',encoding_errors='ignore',lineterminator='\r')
+    return(tracks)
+
+def marquee():
+    marquee = pd.read_csv("Marquee.csv",on_bad_lines="skip",encoding='utf-8',encoding_errors='ignore',lineterminator='\r')
+    return(marquee)
 
 ## page configuration
 st.set_page_config(layout="wide",page_title = "melody meets metrics")
@@ -15,6 +31,14 @@ def home():
     print("I am in Home")
     st.write("#### :green[My Dashboard]")
     myAlbums = load_albums()
+    myTracks = load_tracks()
+    myArtists = load_artists()
+    h_c1, h_c2, h_c3 = st.columns([33,33,33])
+    
+    with h_c1:
+        st.write(":green[My Albums]")
+        with st.container(border=True,height=650):
+            select_album = st.selectbox("Album : ",myAlbums, index=None)
 
 
 
@@ -22,10 +46,3 @@ with tab1:
     home()
 
 
-def load_albums():
-    albums = pd.read_csv("myAlbums.csv")
-    return(albums)
-
-def load_artists():
-    artists = pd.read_csv("myArtists.csv")
-    return(artists)
