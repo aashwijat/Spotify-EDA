c5154b7f055aa866f377d38f6822eb43939d842e
Aashwija Thantry <aashwija.thantry@gmail.com>
2024-10-21 12:46:40 +0530

    most streamed track(s)

--- home.py
+++ home.py
@@ -28,5 +28,30 @@ def home():
                              )
         with c3:
             with st.container(border=True):
-                st.write("##### Top Streamed Songs")
-                sql_query=""
+                sql_query="SELECT \"trackName\",SUM(\"minPlayed\") FROM "\
+                          "\"spotifyEDA.app\".streaming_history "\
+                          "GROUP BY \"trackName\" ORDER BY SUM(\"minPlayed\") DESC "\
+                          "LIMIT 5"
+                get_data = cDB.db_get_data_for_metric(sql_query)
+                st.metric('##### :green[Top Streamed Song]',get_data['trackName'])
+
+                # fig = px.pie(get_data,values='count',names='trackName',title='Top Streamed Songs',
+                #              color_discrete_sequence=('#FFB5B5','#ACFF00'))
+                # st.plotly_chart(fig,theme=None)
+                
+                # st.bar_chart(get_data,x='trackName',y='count',
+                #              x_label='Streaming Count',
+                #              y_label='Track Name',
+                #              horizontal=False,
+                #              color="#FFF7B5")
+            #     fig = px.pie(chart_data, values='disc_count', names='id',title='Discontinued Medicines',
+            #                 color_discrete_sequence=('#FF0000','#0000FF'))
+            # st.plotly_chart(fig,theme=None)
+            with st.expander("##### My Top Tracks"):
+                #st.write("##### TOP 20")
+                sql_query="SELECT \"trackName\",SUM(\"minPlayed\") FROM "\
+                          "\"spotifyEDA.app\".streaming_history "\
+                          "GROUP BY \"trackName\" ORDER BY SUM(\"minPlayed\") DESC "\
+                          "LIMIT 20"
+                get_data = cDB.db_get_data_for_chart(sql_query)
+                st.data_editor(get_data['trackName'],width=400,hide_index=True)
