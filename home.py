3e4831c45fca594d8c4937e2d3f7feab7dc7d9af
Aashwija Thantry <aashwija.thantry@gmail.com>
2024-10-21 10:14:08 +0530

    top artists - home tab

--- home.py
+++ home.py
@@ -13,7 +13,11 @@ def home():
         c0, c1, c2, c3 = st.columns([10,40,40,10])
 
         with c1:
-            with st.container(border=True, height=500):
+            with st.container(border=True, height=550):
                 st.write("Top Streamed Artists : ")
-                sql_query = "SELECT DISTINCT artist FROM"
+                sql_query = "SELECT \"artistName\", SUM(\"minPlayed\") FROM \"spotifyEDA.app\".streaming_history "\
+                            "GROUP BY \"artistName\" ORDER BY SUM(\"minPlayed\") DESC "\
+                            "LIMIT 5"
+                get_data = cDB.db_get_data_for_chart(sql_query)
+                st.data_editor(get_data['artistName'],height=525, use_container_width=True, hide_index=True)
 
