235f60f6c0bec78df924869f2128d710f386a2ea
Aashwija Thantry <aashwija.thantry@gmail.com>
2024-10-21 11:51:16 +0530

    top artists 5

--- home.py
+++ home.py
@@ -7,17 +7,26 @@ import plotly.express as px
 def home():
     print("In HOME")
     with st.container():
-        st.write("My Dashboard")
+        st.write("### :green[My Dashboard]")
     
     with st.container():
-        c0, c1, c2, c3 = st.columns([10,40,40,10])
-
+        c1,c2,c3=st.columns([60,5,35])
         with c1:
-            with st.container(border=True, height=550):
-                st.write("Top Streamed Artists : ")
+            with st.container(border=True):
+                st.write("##### Top Streamed Artists")
                 sql_query = "SELECT \"artistName\", SUM(\"minPlayed\") FROM \"spotifyEDA.app\".streaming_history "\
                             "GROUP BY \"artistName\" ORDER BY SUM(\"minPlayed\") DESC "\
                             "LIMIT 5"
                 get_data = cDB.db_get_data_for_chart(sql_query)
-                st.data_editor(get_data['artistName'],height=525, use_container_width=True, hide_index=True)
-
+                #st.data_editor(get_data['artistName'],height=525, use_container_width=True, hide_index=True)
+                st.bar_chart(get_data,x='artistName',y='sum',
+                             y_label="Minutes Played",
+                             x_label="Artist",
+                             horizontal=False,
+                             height = 500,
+                             color = '#FFC2B5'
+                             )
+        with c3:
+            with st.container(border=True):
+                st.write("##### Top Streamed Songs")
+                sql_query=""
